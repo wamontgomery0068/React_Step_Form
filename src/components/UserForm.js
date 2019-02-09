5aa8123f657a9ebf08f6382b903c099cbf718c73
Your Name <wamontgomery0068@gmail.com>
2019-02-09 11:42:09 -0600

    Set Up State

---
+++ src/components/UserForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+  state = {
+      step: 1,
+      firstName: '',
+      lastName: '',
+      email: '',
+      occupation: '',
+      city: '',
+      bio: ''
+    }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default UserForm
